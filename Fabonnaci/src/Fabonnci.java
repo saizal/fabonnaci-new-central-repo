4f9e1563bf205bc02a81c978b980f461e53233e1
HP <HP@192.168.1.10>
2021-01-03 15:48:07 +0530

    Fabonnaci new

---
+++ Fabonnaci/src/Fabonnci.java
@@ -0,0 +1,26 @@
+
+
+public class Fabonnci {
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		int c;
+
+		System.out.println("print value of"+a);
+		System.out.println("print value of "+b);
+
+	for(int i=1;i<10;i++);
+		
+			{
+				c=a+b;
+				System.out.println("print value of"+c);
+						
+						a=b;
+						b=c;
+			}
+	}
+}
+				
+	
+
+
